e6b1cc9385702c57498cfd29145feb9b3dd38316
Claudiapurba <claudiapurba30@gmail.com>
2025-05-24 00:03:11 +0700

    update color to red

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline bg-red-100">
+    <h1 className="text-3xl font-bold underline bg-red-500">
       Hello world!
     </h1>
   )
